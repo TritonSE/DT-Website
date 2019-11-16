5203f4d02088713a071529b4525a7aa28f306ebb
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2019-11-16 15:51:59 -0800

    image shows up! Addressed minor spacing issues

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import {Navbar,Nav,NavDropdown,Form,FormControl} from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import {Navbar,Nav,Image} from 'react-bootstrap';
 import '../css/Toolbar.css'
 
 class Toolbar extends Component {
   render() {
   return(
     <Navbar bg = "custom" expand="lg" fixed = "top">
-  <Navbar.Brand href="/home">Dynamics Dance Team</Navbar.Brand>
+    <Navbar.Brand href="#home">
+    <img 
+      src = {require("../images/logo1.png")}
+      style={{width:100, marginTop: -7}} 
+      alt = "Could not load"
+      />
+    </Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
       <Nav.Link href="/Home">
-        <d1 className="Home-Header"> Home </d1>
+        < h1 className="Home-Header"> Home </h1>
         </Nav.Link>
       <Nav.Link href="/About">
-        <d2 className = "About-Header"> About </d2>
+        <h2 className = "About-Header"> About </h2>
         </Nav.Link>
       <Nav.Link href="/Support">
-        <d3 className = "Support-Header">Support</d3></Nav.Link>
+        <h3 className = "Support-Header">Support</h3></Nav.Link>
       <Nav.Link href="/Contact"> 
-      <d4 className = "Contact-Header">Contact</d4>
+      <h4 className = "Contact-Header">Contact</h4>
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
