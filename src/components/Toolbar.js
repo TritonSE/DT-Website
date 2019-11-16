00facc224c321c2d79c58c7e04c18f85b5da988a
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2019-11-16 12:02:56 -0800

    gave links custom coloring  support

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import {Navbar,Nav,NavDropdown,Form,FormControl} from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import '../css/Toolbar.css'
 
 class Toolbar extends Component {
   render() {
   return(
-    <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar bg = "custom" expand="lg">
+  <Navbar.Brand href="/home">Dynamics Dance Team</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+    <Nav className="ml-auto">
+      <Nav.Link href="/Home">
+        <d1 className="Home-Header"> Home </d1>
+        </Nav.Link>
+      <Nav.Link href="/About">
+        <d2 className = "About-Header"> About </d2>
+        </Nav.Link>
+      <Nav.Link href="/Support">
+        <d3 className = "Support-Header">Support</d3></Nav.Link>
+      <Nav.Link href="/Contact"> 
+      <d4 className = "Support-Contact">Contact</d4>
+      </Nav.Link>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
   </Navbar.Collapse>
 </Navbar>
 );
