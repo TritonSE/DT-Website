9815d64661306f2fe0680ffc1358537d3474d97a
Jason Cabrera <cabrerajason7@gmail.com>
2019-11-18 11:58:58 -0800

    added more pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Route } from 'react-router-dom';
 // importing pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Support from './pages/Support';
+import Contact from './pages/Contact';
 
 class App extends Component {
 	render() {
@@ -13,6 +15,8 @@ class App extends Component {
     		<div>
 				<Route path="/" exact render={(props) => <Home {...props}/>}/>
 				<Route path="/about" exact render={(props) => <About {...props}/>}/>
+				<Route path="/support" exact render={(props) => <Support {...props}/>}/>
+				<Route path="/contact" exact render={(props) => <Contact {...props}/>}/>
    	 	</div>
   		);
   }
