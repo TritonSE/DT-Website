8c78a6c5342342b068415aba827fb339c1bcce4b
Amrit-S <aksingh@ucsd.edu>
2019-11-20 13:27:58 -0800

    Blinking Made Static

--- src/components/BlinkingText.js
+++ src/components/BlinkingText.js
@@ -30,12 +30,12 @@ class BlinkingText extends Component {
             }
           }
         //changeColor of "Dynamics" every 1 second
-        setInterval(changeColor, 1000)
+        //setInterval(changeColor, 1000)
 
 		// component will render JSX (react's version of html) inside return statement
 		return (
             <div className="Body-Header">
-            <p id="bulletTxt"> {entities.decode('&#8226;')} <p style={{paddingLeft: 10}}id="dynamicsTxt"> Dynamics </p> </p>
+            <p id="bulletTxt" style={{color: "#7fffd4"}}> {entities.decode('&#8226;')} <p style={{color: "#E42B6E" , paddingLeft: 10}}id="dynamicsTxt"> Dynamics </p> </p>
             <p style={{paddingLeft: 10}}> Dance </p>
             <p style={{paddingLeft: 10}}> Team </p>
         </div>
