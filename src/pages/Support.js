ae50cfecd202d98a417351ae2ac58195df82f10a
Amrit-S <aksingh@ucsd.edu>
2019-12-02 21:45:53 -0800

    Base Formatting Done

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Toolbar from '../components/Toolbar.js';
+import '../css/Support.css';
+import { SegmentInline } from 'semantic-ui-react';
 
 class Support extends Component {
 	render() {
+		var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.\n\nAliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu.";
 		return (
 			<div>
 			<Toolbar />
+			<div style={{display: 'flex', alignItems: 'center'}}>
+			<body className="Support-Text">
+				{text}
+			</body>
+			<header className="Header-Text">
+				How To Contribute
+			</header>
+			</div>
 			</div>
 		);
 	}
