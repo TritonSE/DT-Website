6ecc9c86e49d0f0e76f3edc208a4b68ec017853a
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2019-12-23 16:32:00 -0800

    email changes made and minor spacing edits

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,8 @@ class Contact extends Component {
 			<div>
 				<Toolbar />
 			<div className = "number"> 
-			<a href = "mailto:dynamicsperformanceteam@gmail.com" target="blank" >dynamicsperformanceteam@gmail.com</a>
+			<a href = "mailto:dynamicsperformanceteam@gmail.com" target="blank" >Email</a>
+			<br></br>
 			<a href = "https://www.facebook.com/DynamicsPerformanceTeam/" target = "blank">  Facebook </a>
 			<br></br>
 			<a href = "https://www.instagram.com/dynamicsperformanceteam/?hl=en" target = "blank"> Instagram </a>
