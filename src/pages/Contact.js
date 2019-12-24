cb03d8df78cb924b77d94c9f0d3a0163ef7dfa43
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2019-12-23 16:24:26 -0800

    added the links for eveything and minor spacing changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,6 +8,11 @@ class Contact extends Component {
 				<Toolbar />
 			<div className = "number"> 
 			<a href = "mailto:dynamicsperformanceteam@gmail.com">dynamicsperformanceteam@gmail.com</a>
+			<a href = "https://www.facebook.com/DynamicsPerformanceTeam/"> Facebook </a>
+			<br></br>
+			<a href = "https://www.instagram.com/dynamicsperformanceteam/?hl=en"> Instagram </a>
+			<br></br>
+			<a href = "https://www.yelp.com/biz/dynamics-performance-team-san-jose"> Yelp </a>
 			</div>
 			<div className = "reach">
 				Reach Out
