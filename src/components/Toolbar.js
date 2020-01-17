385d8b2b6e4e1835f15930f2096b3e40d5ff8a3d
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2020-01-17 00:16:56 -0800

    cleaned up uneccasry code and it all works now

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -6,112 +6,13 @@ import SocialFollow from './SocialFollow';
 import { NavLink, Link } from 'react-router-dom';
 import '../css/Toolbar.css'
 
-var pageArray = [true, false, false, false];
-var homeClass = "activeHome";
-var aboutClass = "activeAbout";
-var supportClass = "activeSupport";
-var contactClass = "activeContact";
-
 class Toolbar extends Component {
-
-  constructor(){
-    super()
-    this.handleAboutRedirect = this.handleAboutRedirect.bind(this);
-    this.changeArray = this.changeArray.bind(this);
-    this.changeClass = this.changeArray.bind(this);
-  }
    
-
-  //function to handle page redirects using React router (see App.js)
-  handleAboutRedirect = page => event => {
-
-		event.preventDefault();
-    this.props.history.push("" + page);	// redirects to specified page
-  }
-  
-  changeArray(page){
-    if(page === "../pages/home")
-    {
-      pageArray[0] = true;
-      pageArray[1] = false;
-      pageArray[2] = false;
-      pageArray[3] = false;
-    }
-    if(page === "/about")
-    {
-      pageArray[1] = true;
-      pageArray[0] = false;
-      pageArray[2] = false;
-      pageArray[3] = false;
-    }
-    if(page === "/support")
-    {
-      pageArray[2] = true;
-      pageArray[0] = false;
-      pageArray[1] = false;
-      pageArray[3] = false;
-    }
-    if(page === "../pages/contact")
-    {
-      pageArray[0] = false;
-      pageArray[1] = false;
-      pageArray[2] = false;
-      pageArray[3] = true;
-
-    }
-
-    console.log(page);
-    for(var y = 0; y< pageArray.length; y++){
-      console.log(y + " " + pageArray[y]);
-    }
-
-    this.handleAboutRedirect(page)
-
-    //this.changeClass()
-  }
-
-  changeClass(){
-    if(pageArray[0])
-    {
-      homeClass = "activeHome";
-      aboutClass = "About-Header";
-      supportClass = "Support-Header";
-      contactClass = "Contact-Header";
-    }
-    else{
-      if(pageArray[1]){
-        homeClass = "Home-Header";
-        aboutClass = "activeAbout";
-        supportClass = "Support-Header";
-        contactClass = "Contact-Header";
-      }
-      else{
-        if(pageArray[2]){
-          homeClass = "Home-Header";
-          aboutClass = "About-Header";
-          supportClass = "activeSupport";
-          contactClass = "Contact-Header";
-        }
-        else{
-          homeClass = "Home-Header";
-          aboutClass = "About-Header";
-          supportClass = "Support-Header";
-          contactClass = "activeContact";
-        }
-      }
-    }
-  }
-
   render() {
-    const aboutPage = "/about";
-		const supportPage = "/support";
-    const contactPage = "../pages/contact";
-    const homePage = "../pages/home"
-
   return(
     <div>
       <Navbar bg = "custom" expand="lg">
-      <Navbar.Brand href="/Home" onClick = {this.changeArray(homePage)}>
+      <Navbar.Brand href="/Home">
         {/** Image/Text for dynamics team*/}
         <LogoText/>
       </Navbar.Brand>
@@ -120,46 +21,63 @@ class Toolbar extends Component {
           <Nav className="ml-auto">
 
             {/** Home part of Navbar */}
-            <NavLink  to="/" href = "/Home" exact activeClassName = "active" className= "Home-Header" onClick = {this.changeArray(homePage)}
-             style={{
-              fontWeight: "bold",
-              color: "black",
-            }} activeStyle={{
-              color: "#FF4081"
-            }}> 
-                  Home
-              </NavLink>
+            <NavLink  
+              to="/" 
+              href = "/Home" 
+              exact activeClassName = "active" 
+              className= "Home-Header" 
+              style={{
+                fontWeight: "bold",
+                color: "black",
+              }} 
+              activeStyle={{
+                color: "#FF4081"
+              }}> 
+                Home
+            </NavLink>
 
             {/** About part of Navbar */}
-            <NavLink to= "/about" href="/About" className = "About-Header" onClick = {this.changeArray(aboutPage)}
-            style={{
-              fontWeight: "bold",
-              color: "black"
-            }}activeStyle={{
-              color: "#FF4081"
-            }} > 
+            <NavLink 
+              to= "/about"
+              href="/About" 
+              className = "About-Header" 
+              style={{
+                fontWeight: "bold",
+                color: "black"
+              }}
+              activeStyle={{
+                color: "#FF4081"
+              }} > 
                 About 	
-              </NavLink>
+            </NavLink>
 
             {/** Support part of Navbar */}
-            <NavLink to = "/support" href="/Support" className = "Support-Header" onClick = {this.changeArray(supportPage)}
-            style={{
-              fontWeight: "bold",
-              color: "black"
-            }}activeStyle={{
-              color: "#FF4081"
-            }}>
+            <NavLink 
+              to = "/support" 
+              href="/Support" 
+              className = "Support-Header" 
+              style={{
+                fontWeight: "bold",
+                color: "black"
+              }}
+              activeStyle={{
+                color: "#FF4081"
+              }}>
                 Support
             </NavLink>
 
             {/** Contact part of Navbar */}
-            <NavLink to = "/contact" href="/Contact" className = "Contact-Header" onClick = {this.changeArray(contactPage)}
-            style={{
-              fontWeight: "bold",
-              color: "black",
-            }}activeStyle={{
-              color: "#FF4081"
-            }}>
+            <NavLink 
+              to = "/contact" 
+              href="/Contact" 
+              className = "Contact-Header" 
+              style={{
+                fontWeight: "bold",
+                color: "black",
+              }}
+              activeStyle={{
+                color: "#FF4081"
+              }}>
                 Contact
             </NavLink>
 
