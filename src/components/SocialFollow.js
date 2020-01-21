029c26086e2ae2caa871788029685a5c1bda1031
Uposhanto <uposhanto.b@gmail.com>
2020-01-20 21:38:35 -0800

    deleting actual modal code so it's just icon code

--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -34,44 +34,12 @@ class SocialFollow extends Component{
                   </a>
                 </li>
                 <li>
-                  <Button variant="primary" onclick='Example()'>
-                    Subscribe
-                  </Button>
+                  <a href="https://www.mailchimp.com">Subscribe</a>
                 </li>
               </ul>
             </div>
         );
       } 
-}
-
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <a href="#" onClick={handleShow}>
-        Launch demo modal
-      </a>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
 }
 
 export default SocialFollow;
