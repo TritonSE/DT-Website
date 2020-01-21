e6395d441d43ce248406501d6afa736ec87a912a
Jason Cabrera <cabrerajason7@gmail.com>
2020-01-20 23:20:33 -0800

    Updates

---
+++ precache-manifest.8a669abc7ea603f42ebc6b3d36f94731.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8f83cccca90fd180f8b80f3b0d5dc1",
+    "url": "/dynamics-website/index.html"
+  },
+  {
+    "revision": "af1e56d2d65ec9a7de02",
+    "url": "/dynamics-website/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "ee7184c07d7354839e61",
+    "url": "/dynamics-website/static/css/main.14a22d78.chunk.css"
+  },
+  {
+    "revision": "af1e56d2d65ec9a7de02",
+    "url": "/dynamics-website/static/js/2.f36fa463.chunk.js"
+  },
+  {
+    "revision": "ee7184c07d7354839e61",
+    "url": "/dynamics-website/static/js/main.03284363.chunk.js"
+  },
+  {
+    "revision": "144a39a500ca3f32b690",
+    "url": "/dynamics-website/static/js/runtime-main.c507a37b.js"
+  },
+  {
+    "revision": "9fec259f4c4d5cf845e97370a3e8fc15",
+    "url": "/dynamics-website/static/media/dancer.9fec259f.jpg"
+  },
+  {
+    "revision": "e8fae51aa300c89408e2a2aa33b1d878",
+    "url": "/dynamics-website/static/media/logo1.e8fae51a.png"
+  },
+  {
+    "revision": "8178c2a12b384d0ad00ffd25bcacbc7d",
+    "url": "/dynamics-website/static/media/samplepic.8178c2a1.jpg"
+  }
+]);
