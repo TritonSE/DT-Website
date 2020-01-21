8d07f7463721418601b03ac01cdaf9890181b04a
Jason Cabrera <cabrerajason7@gmail.com>
2020-01-20 23:06:46 -0800

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Support from './pages/Support';
@@ -13,7 +13,7 @@ class App extends Component {
     		<div>
 				<Route path="/" exact render={(props) => <Home {...props}/>}/>
 				<Route path="/home" exact render={(props) => <Home {...props}/>}/>
-				<Route path="/about" exact render={(props) => <About {...props}/>}/>
+				<Route path="/about" exact component={About}/>
 				<Route path="/support" exact render={(props) => <Support {...props}/>}/>
 				<Route path="/contact" exact render={(props) => <Contact {...props}/>}/>
    	 	</div>
