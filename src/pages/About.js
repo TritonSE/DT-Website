35578eed2b597e1f7d499ca4b6b8e213666caaf7
Dhanush <dhanunanjunda2@gmail.com>
2020-02-20 21:21:16 -0800

    added 4 pages in about

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,57 +2,20 @@ import React, { Component } from 'react';
 import Toolbar from '../components/Toolbar.js';
 import { Container, Col, Row } from 'react-bootstrap';
 import '../css/About.css';
-import Members from '../components/Members.js';
+import Mission from '../pages/Mission.js';
+import Performers from '../pages/Performers.js';
+import Join from '../pages/Join.js';
+import Directors from '../pages/Directors.js';
 
 class About extends Component {
 	render() {
 		return (
 			<div>
-			{/* FOR MOBILE */}
-			<div className="mobile">
-			<Toolbar/>
-			<Container>
-				<Col>
-					<h1 className="Title"> Our Mission </h1>
-					<h2 className="Content1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </h2>
-					<h2 className="Content2"> Aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu. </h2>
-					<h1 className="Title2"> The Lineup </h1>
-					<h2 className="Content4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h2>
-				</Col>
-				<Members/>
-				<br/>
-				<br/>
-			</Container>
-			</div>
-			<div className="web">
-			<Toolbar/>
-			<Container>
-			<div className="div">
-			<Row>
-			<Col>
-				<h1 className="Title"> Our Mission </h1>
-			</Col>
-			<Col>
-				<h1 className="Content1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </h1>
-				<h2 className="Content2"> incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis.</h2>
-				<h3 className="Content3"> Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu.</h3>
-			</Col>
-			</Row>
-			<Row>
-				<Col>
-					<h1 className="Content4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h1>
-				</Col>
-				<Col>
-					<h1 className="Title2"> The Lineup </h1>
-				</Col>
-			</Row>
-				<Members/>
-			</div>
-			<br/>
-			<br/>
-			<br/>
-			</Container>
-			</div>
+				<Toolbar/>
+				<Mission/>
+				<Directors/>
+				<Performers/>
+				<Join/>
 			</div>
 		);
 	}
