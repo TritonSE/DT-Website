d8f6c0e9194737c79e4b0df835d7571d5f058154
Uposhanto Bhattacharya <37908451+ubhattac@users.noreply.github.com>
2020-05-06 16:13:00 -0700

    Update Audition.js

--- src/pages/Audition.js
+++ src/pages/Audition.js
@@ -1,3 +1,9 @@
 import React, { Component } from 'react';
 import Toolbar from '../components/Toolbar.js';
 import { Container, Col, Row } from 'react-bootstrap';
+
+class Audition extends Component {
+    render() {
+        
+    }
+}
