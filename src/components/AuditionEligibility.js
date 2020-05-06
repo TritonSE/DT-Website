0197a02545c5a713d4336a1260bf1ece9dc8a0a5
Uposhanto Bhattacharya <37908451+ubhattac@users.noreply.github.com>
2020-05-06 16:36:01 -0700

    Update AuditionEligibility.js

--- src/components/AuditionEligibility.js
+++ src/components/AuditionEligibility.js
@@ -8,23 +8,25 @@ class AuditionEligibility extends Component {
                 
                 <div>
                 <ul>
-                    <li>first item</li>
-                    <li>second item</li>
-                    <li>third item</li>
+                    <li>12+ years old</li>
+                    <li>One year of dance experience.</li>
+                    <li>3 hours minimum technique at one or more dance studios: one hour of jazz, 
+                        one hour of ballet, and one hour of another genre of dance.</li>
                 </ul>
                 </div>
 
                 <div>
                 <ul>
-                    <li>first item</li>
-                    <li>second item</li>
-                    <li>third item</li>
+                    <li>8-10 years old</li>
+                    <li>One year of dance experience.</li>
+                    <li>3 hours minimum technique at one or more dance studios: one hour of jazz, 
+                        one hour of ballet, and one hour of another genre of dance.em</li>
                 </ul>
                 </div>
 
                 <div>
                 <ul>
-                    <li>first item</li>
+                    <li>5-7 years old</li>
                     <li>second item</li>
                     <li>third item</li>
                 </ul>
