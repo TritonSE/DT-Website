d31cae9d9f4a226daed50765b20143680c201cc9
Uposhanto Bhattacharya <37908451+ubhattac@users.noreply.github.com>
2020-05-06 20:47:25 -0700

    removed compile warnings

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Toolbar from '../components/Toolbar.js';
-import { Container, Col, Row } from 'react-bootstrap';
 import '../css/About.css';
 import Mission from '../pages/Mission.js';
 import Performers from '../pages/Performers.js';
