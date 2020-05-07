41480d2fd0e80c22171e0ccdfa569a0200616e00
Uposhanto Bhattacharya <37908451+ubhattac@users.noreply.github.com>
2020-05-06 19:26:41 -0700

    90% done with Audition page

--- src/components/AuditionImages.js
+++ src/components/AuditionImages.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import Image from 'react-bootstrap/Images';
+import Image from 'react-bootstrap/Image';
+import first from '../images/audition/first.jpg';
+import second from '../images/audition/second.jpg';
+import '../css/Audition.css';
 
 class AuditionImages extends Component {
     render() {
         return(
             <div>
                 <div>
-                <Image src=""/>
+                <Image src={first} alt="Couldn't load IMG" className="auditionIMG"/>
             </div>
             <div>
-                <Image src=""/>
+                <Image src={second} alt="Couldn't load IMG" className="auditionIMG"/>
             </div>
             </div>
         );
