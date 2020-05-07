d9fb17d06aeec39584b1d7d606a95185d4667622
Uposhanto Bhattacharya <37908451+ubhattac@users.noreply.github.com>
2020-05-06 19:19:47 -0700

    added images component again

---
+++ src/components/AuditionImages.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Image from 'react-bootstrap/Images';
+
+class AuditionImages extends Component {
+    render() {
+        return(
+            <div>
+                <div>
+                <Image src=""/>
+            </div>
+            <div>
+                <Image src=""/>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default AuditionImages;
--- src/pages/Audition.js
+++ src/pages/Audition.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Image from 'react-bootstrap/Image';
 import Toolbar from '../components/Toolbar.js';
+import AuditionImages from '../components/AuditionImages.js';
 import AuditionInfo from '../components/AuditionInfo.js';
 import AuditionEligibility from '../components/AuditionEligibility.js';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -14,17 +14,16 @@ class Audition extends Component {
                 <Row>
 
                 <Col>
-                    <Image src="../images/audition/first.jpg"></Image>
-                    <Image src="../images/audition/second.jpg"></Image>
+                    <AuditionImages />
                 </Col>
 
                 <Col className="audition-text">
                     <Container>
-                        <AuditionInfo></AuditionInfo>
+                        <AuditionInfo/>
                     </Container>
 
                     <Container>
-                        <AuditionEligibility></AuditionEligibility>
+                        <AuditionEligibility/>
                     </Container>
                 </Col>
                 </Row>
