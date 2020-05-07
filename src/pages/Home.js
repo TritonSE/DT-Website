b1b39c7e72d1be16fd228ed85c0f85aca8135ea1
ubhattac <37908451+ubhattac@users.noreply.github.com>
2020-05-06 20:53:05 -0700

    disable right click for images (#47)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -52,10 +52,10 @@ class Home extends React.Component {
 			// can only return one element so if you want more than one make sure they're nested inside a div
 			<div>
 				<Toolbar />
-			<div className="text" >
+			<div className="text" onContextMenu={(e)=> e.preventDefault()}>
 				<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'></link>
 				<p>A team of <span style={{color: '#FF4081',fontStyle: 'normal',fontWeight: 'normal'}}id='dynamics' ref={this.myRef}> <br/>dynamic</span> <span><br/>dancers.</span></p>
-				<img className='danceimg' src={dancer} style={{transform: "rotate(5deg)"}} ref = {this.imageRef}/>
+				<img  className='danceimg' src={dancer} style={{transform: "rotate(5deg)"}} ref = {this.imageRef}/>
 			</div>
 			</div>
 		);
