819e057d57aeb904035f31aeb4e57767c7f51e61
nvanny <53798448+nvanny@users.noreply.github.com>
2020-05-26 20:51:57 -0700

    Ways to give 2 (#59)
    * ways to give branch new
    
    * fixed slideshow
    
    * fixed some header issues

--- src/components/SupportStates.js
+++ src/components/SupportStates.js
@@ -75,7 +75,7 @@ class SupportStates extends Component {
         <div className="Proceed-Style">
           <text
             className="Amount-Text"
-            style={{bottom: "3%", fontWeight: "bold"}}
+            style={{bottom: "3%", fontWeight: "bold", marginLeft: "-5%"}}
           >
             {"Amount: $"}
           </text>
@@ -88,7 +88,7 @@ class SupportStates extends Component {
             onChange={this.handleUpdateAmount}
           >
           </input>
-          <IoIosAlert style={{display: this.state.displayError ? null:'none', color:"red", width: "5.5%", maxHeight:"5.5%"}}/>
+          <IoIosAlert style={{display: this.state.displayError ? null:'none', color:"red", width: "2%", maxHeight:"2%", marginTop: "2%", marginLeft:"1%"}}/>
           <button 
             style={{left: "53%"}} 
             className="btn btn-secondary Proceed-Button"
@@ -122,6 +122,7 @@ class SupportStates extends Component {
             </input>
             <button 
               className="btn btn-secondary Proceed-Button"
+              style={{marginTop: "7%"}}
               onClick={this.handleAmountChange}
             >
               {"Adjust"}
