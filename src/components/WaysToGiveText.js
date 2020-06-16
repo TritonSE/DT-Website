2fe5929222b9eb252d5a4b5f32a7599b292a5234
nvanny <nvanny@ucsd.edu>
2020-06-15 21:53:54 -0700

    updated the font size

--- src/components/WaysToGiveText.js
+++ src/components/WaysToGiveText.js
@@ -7,11 +7,39 @@ import Instructions from '../images/waytogive/amazon smile instructions.jpg';
 import ModalImage from 'react-modal-image';
 
 class WaysToGiveText extends Component {
+    constructor(props){
+		super(props);
+		this.myRef = React.createRef();
+		this.imageRef = React.createRef();
+		this.state = {width: 0, height: 0, isMobile: false};
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+	}
+
+	componentDidMount() {
+        window.scrollTo(0,0);
+		this.updateWindowDimensions();
+		window.addEventListener('resize', this.updateWindowDimensions);
+	}
+
+	updateWindowDimensions() {
+		if (window.innerWidth > 600) {
+			this.setState({isMobile: false});
+		}
+		else if (window.innerWidth <= 600) {
+			this.setState({isMobile: true});
+		}
+		this.setState({width: window.innerWidth, height: window.innerHeight});
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateWindowDimensions);
+	}
     render() {
+        if(!this.state.isMobile) {
         return (
             <div className = "bodytxt">
                 <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'></link>
-                <p id="dynamicsTxt"> 
+                <p> 
                 Dynamics Performance Team is proud to be part of the local arts community in the Bay Area for the past 15 years. We are committed to giving youth and young adults the opportunity to train in versatile genres and grow their passion for dance. Performing Arts are an essential part of the community, and with your help, we can continue to offer training to all dancers regardless of financial background.
 
  </p>
@@ -38,6 +66,26 @@ To learn more about our individual or corporate giving programs and benefits,
                  
             </div>
         );
+        }
+        else {
+        return (
+            <div className = "bodytxt">
+                <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'></link>
+                <p> 
+                Dynamics Performance Team is proud to be part of the local arts community in the Bay Area for the past 15 years. We are committed to giving youth and young adults the opportunity to train in versatile genres and grow their passion for dance. Performing Arts are an essential part of the community, and with your help, we can continue to offer training to all dancers regardless of financial background.
+
+ </p>
+ <p>
+Dynamics Performance Team is a publicly Funded 501(c)(3) non-profit organization that depends on our generous donors, corporate sponsors, and friends. Your tax-deductible gift allows Dynamics to present quality performances, keep rates low, and offer scholarships to our dancers—affordability should not be an obstacle in the way of our dancers’ dreams. Your donation also allows for Dynamics dancers to compete, train at conventions, travel to nationals, and purchase costumes.
+</p>
+<p>
+To learn more about our individual or corporate giving programs and benefits, 
+<a href = {SponsorLetter} target = "_blank"> <span style={{color: '#FF4081',fontStyle: 'normal',fontWeight: 'normal', textDecorationLine: 'underline'}}>please read our sponsor letter here </span></a>, or contact us at dynamicsperformanceteam@gmail.com. You can also give us a call at (408) 622-0249.
+</p>
+                 
+                 </div>
+        );
+        }
     }
 }
 
