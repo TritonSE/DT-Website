2a9ec574107b9003dbe77366e7fb88229baad8a1
Dhanush <dhanunanjunda2@gmail.com>
2020-06-17 15:23:35 -0700

    changed css names

--- src/pages/Dropdown/Mission.js
+++ src/pages/Dropdown/Mission.js
@@ -14,10 +14,10 @@ class Mission extends React.Component{
 			{/* FOR MOBILE */}
 			<div className="Mobile">
 			<Container>
-					<h1 className="title1"> Our Mission </h1>
-					<img className="Image" src={image} alt="could not load"/>
-					<h1 className="Content1"> The specific purpose of this organization is to inspire, educate, and promote passion, dedication, and performance within the art of dance.  All funds, whether income or principal, and whether acquired by gift or contribution or otherwise, shall be devoted to said purposes. </h1>
-					<h2 className="content2"> To donate to our non-profit, please 
+					<h1 className="Mission-Title"> Our Mission </h1>
+					<img className="Dancers-Image" src={image} alt="could not load"/>
+					<h1 className="Mission-Paragraph"> The specific purpose of this organization is to inspire, educate, and promote passion, dedication, and performance within the art of dance.  All funds, whether income or principal, and whether acquired by gift or contribution or otherwise, shall be devoted to said purposes. </h1>
+					<h2 className="Support-link"> To donate to our non-profit, please 
 					<Link to="/support" className="linked"> click here! </Link>
 					</h2>
 			</Container>
@@ -27,14 +27,14 @@ class Mission extends React.Component{
 			<div className="div">
 				<Row>
 				<Col className="column1">
-					<h1 className="Title"> Our Mission </h1>
-					<h1 className="Content1"> The specific purpose of this organization is to inspire, educate, and promote passion, dedication, and performance within the art of dance.  All funds, whether income or principal, and whether acquired by gift or contribution or otherwise, shall be devoted to said purposes. </h1>
-				<h2 className="Content2"> To donate to our non-profit, please 
+					<h1 className="Mission-Title"> Our Mission </h1>
+					<h1 className="Mission-Paragraph"> The specific purpose of this organization is to inspire, educate, and promote passion, dedication, and performance within the art of dance.  All funds, whether income or principal, and whether acquired by gift or contribution or otherwise, shall be devoted to said purposes. </h1>
+				<h2 className="Support-Link"> To donate to our non-profit, please 
 				<Link to="/support" className="linked"> click here! </Link>
 				</h2>
 				</Col>
 				<Col xs={7} className="column2">
-				<img className="Image" src={image} alt="could not load"/>
+				<img className="Dancers-Image" src={image} alt="could not load"/>
 				</Col>
 				</Row>
 			</div>
