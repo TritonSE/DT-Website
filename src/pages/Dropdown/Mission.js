dac8d6b0e42a00912524fd830004525abc090ddc
dnanjunda <57281768+dnanjunda@users.noreply.github.com>
2020-06-18 03:46:35 -0700

    Mobile about, history, mission pages (#62)
    * about page is done
    
    * change made for new prototype
    
    * merged content with dummy pages
    
    * increased container width for about page
    
    * changed font size of header in about page
    
    * fixed issue with image when resizing
    
    * fixed container width
    
    * made container fluid
    
    * mobile pages are done
    
    * fixed merge conflicts
    
    * added image to history page
    
    * changed css names
    
    Co-authored-by: Jason Cabrera <cabrerajason7@gmail.com>

--- src/pages/Dropdown/Mission.js
+++ src/pages/Dropdown/Mission.js
@@ -12,35 +12,32 @@ class Mission extends React.Component{
             <div>
 				<Toolbar bool={true}/>
 			{/* FOR MOBILE */}
-			<div className="mobile">
+			<div className="Mobile">
 			<Container>
-				<Col>
-					<h1 className="Title"> Our Mission </h1>
-					<h2 className="Content1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </h2>
-					<h2 className="Content2"> Aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu. </h2>
-				</Col>
-				<br/>
-				<br/>
+					<h1 className="Mission-Title"> Our Mission </h1>
+					<img className="Dancers-Image" src={image} alt="could not load"/>
+					<h1 className="Mission-Paragraph"> The specific purpose of this organization is to inspire, educate, and promote passion, dedication, and performance within the art of dance.  All funds, whether income or principal, and whether acquired by gift or contribution or otherwise, shall be devoted to said purposes. </h1>
+					<h2 className="Support-link"> To donate to our non-profit, please 
+					<Link to="/support" className="linked"> click here! </Link>
+					</h2>
 			</Container>
 			</div>
-			<div className="web">
-			<Container>
+			<div className="Web">
+			<Container fluid>
 			<div className="div">
 				<Row>
 				<Col className="column1">
-					<h1 className="Title"> Our Mission </h1>
-					<h1 className="Content1"> The specific purpose of this organization is to inspire, educate, and promote passion, dedication, and performance within the art of dance.  All funds, whether income or principal, and whether acquired by gift or contribution or otherwise, shall be devoted to said purposes. </h1>
-				<h2 className="Content2"> To donate to our non-profit, please 
+					<h1 className="Mission-Title"> Our Mission </h1>
+					<h1 className="Mission-Paragraph"> The specific purpose of this organization is to inspire, educate, and promote passion, dedication, and performance within the art of dance.  All funds, whether income or principal, and whether acquired by gift or contribution or otherwise, shall be devoted to said purposes. </h1>
+				<h2 className="Support-Link"> To donate to our non-profit, please 
 				<Link to="/support" className="linked"> click here! </Link>
 				</h2>
 				</Col>
 				<Col xs={7} className="column2">
-				<img className="Image" src={image} alt="could not load"/>
+				<img className="Dancers-Image" src={image} alt="could not load"/>
 				</Col>
 				</Row>
 			</div>
-			<br/>
-			<br/>
 			<br/>
 			</Container>
 			</div>
