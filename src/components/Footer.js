5598c481540160bf4894ac270f3833b60f9bd2d1
Amrit-S <aksingh@ucsd.edu>
2020-06-18 22:45:21 -0700

    Updated footer to display tweaked mobile version after a certain width size in desktop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ import '../css/Footer.css';
 
 class Footer extends Component {
 
+    MOBILE_MAX_WIDTH = 1150; //Largest screen size the mobile layout renders for
+
     constructor(props){
 		super(props);
 		this.state = { isMobile: false};
@@ -12,14 +14,15 @@ class Footer extends Component {
 
 	componentDidMount() {
 		window.scrollTo(0,0);
-		this.updateWindowDimensions();
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
 	}
 	
 	updateWindowDimensions() {
-		if (window.innerWidth > 600 && this.state.isMobile){
+		if (window.innerWidth > this.MOBILE_MAX_WIDTH && this.state.isMobile){
 			this.setState({ isMobile: false });
 		}
-		else if(window.innerWidth <= 600 && !this.state.isMobile){
+		else if(window.innerWidth <= this.MOBILE_MAX_WIDTH && !this.state.isMobile){
 			this.setState({ isMobile: true });
 		}
 	}
@@ -30,34 +33,30 @@ class Footer extends Component {
             return (
                 <div className="Footer">
                     <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-                        <h1 className="Contact-Header" style={{fontSize: "5.3vw"}}>
-                            <span style={{color:"black"}}>
+                        <h1 className="Contact-Text">
                                 Contact <br/> Us
-                            </span>
                         </h1>
-                        <h1 className="Contact-Header" style={{textAlign: "left", fontSize: "3.4vw", flex:"1"}}>
-                            <span style={{color:"black"}}>
-                                <a style={{color: "black"}} href={`tel:4086220249`}>+ 408 622 0249  </a><br/> <a style={{color: "black"}} href={`mailto:dynamicsperformanceteam@gmail.com`}>dynamicsperformanceteam@gmail.com</a>
-                            </span>
+                        <h1 className="Contact-Info-Text">
+                                <a style={{color: "black"}} href={`tel:4086220249`}>+ 408 622 0249  </a>
+                                <br/> 
+                                <a style={{color: "black"}} href={`mailto:dynamicsperformanceteam@gmail.com`}>dynamicsperformanceteam@gmail.com</a>
                         </h1>
                     </div>
-                    <div style={{marginTop:"5%"}}>
-                     <SocialIcons/>
+                    <div className="Social-Icon-Formatting">
+                        <SocialIcons/>
                     </div>
                 </div>
             );
         } else {
             return (
                 <div className="Footer">
-                    <h1 className="Contact-Header" style={{fontSize: "2.3vw"}}>
-                        <span style={{color:"black"}}>
+                    <h1 className="Contact-Text">
                             Contact <br/> Us
-                        </span>
                     </h1>
-                    <h1 className="Contact-Header" style={{textAlign: "left", fontSize: "1.5vw"}}>
-                        <span style={{color:"black"}}>
-                            + 408 622 0249 <br/> <a style={{color: "black"}} href={`mailto:dynamicsperformanceteam@gmail.com`}>dynamicsperformanceteam@gmail.com</a>
-                        </span>
+                    <h1 className="Contact-Info-Text">
+                            + 408 622 0249 
+                            <br/> 
+                            <a style={{color: "black"}} href={`mailto:dynamicsperformanceteam@gmail.com`}>dynamicsperformanceteam@gmail.com</a>
                     </h1>
                 </div>
             );
