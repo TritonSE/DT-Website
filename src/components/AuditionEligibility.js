4b246de1943a744f706565a74ebae42b5143ced4
uposhanto <37908451+ubhattac@users.noreply.github.com>
2020-06-26 22:29:59 -0700

    Finished audition page mobile design (#87)
    * audition page mobile design done
    
    * added gallery photos
    
    * changed homepage
    
    * updated homepage to domain name
    
    * changed homepage again
    
    * changed homepage for deployment purposes
    
    * Updated route names (#94)
    
    * global css file is done (#89)
    
    * global css file is done
    
    * all pages import global css
    
    * fixed css for h1
    
    * removed css in individual files and fixed css for h1 and p
    
    * fixed all issues with global css
    
    * updated mission route name in toolbar
    
    Co-authored-by: Jason Cabrera <cabrerajason7@gmail.com>
    
    * made global css file compatible with audition header
    
    Co-authored-by: Jason Cabrera <cabrerajason7@gmail.com>
    Co-authored-by: Jason Cabrera <30588403+jayrc7@users.noreply.github.com>
    Co-authored-by: dnanjunda <57281768+dnanjunda@users.noreply.github.com>

--- src/components/AuditionEligibility.js
+++ src/components/AuditionEligibility.js
@@ -1,41 +1,49 @@
 // component for different levels of audition eligibility
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AuditionEligibility extends Component {
-    render() {
-        return(
-            <div className="colored-bullets font">
-                <div>
-                <h3>DPT Senior Team eligibility:</h3>
-                <ul>
-                    <li>12+ years old</li>
-                    <li>One year of dance experience.</li>
-                    <li>3 hours minimum technique at one or more dance studios: one hour of jazz, 
-                        one hour of ballet, and one hour of another genre of dance.</li>
-                </ul>
-                </div>
+  render() {
+    return (
+      <div className="colored-bullets font">
+        <div>
+          <h3>DPT Senior Team eligibility:</h3>
+          <ul>
+            <li>12+ years old.</li>
+            <li>One year of dance experience.</li>
+            <li>
+              3 hours minimum technique at one or more dance studios: one hour
+              of jazz, one hour of ballet, and one hour of another genre of
+              dance.
+            </li>
+          </ul>
+        </div>
 
-                <div>
-                <h2>Dynamos Junior Team eligibility:</h2>
-                <ul>
-                    <li>8-10 years old</li>
-                    <li>One year of dance experience.</li>
-                    <li>3 hours minimum technique at one or more dance studios: one hour of jazz, 
-                        one hour of ballet, and one hour of another genre of dance.</li>
-                </ul>
-                </div>
+        <div>
+          <h2>Dynamos Junior Team eligibility:</h2>
+          <ul>
+            <li>8-10 years old.</li>
+            <li>One year of dance experience.</li>
+            <li>
+              3 hours minimum technique at one or more dance studios: one hour
+              of jazz, one hour of ballet, and one hour of another genre of
+              dance.
+            </li>
+          </ul>
+        </div>
 
-                <div>
-                <h2>Dynamites Mini Team eligibility:</h2>
-                <ul>
-                    <li>5-7 years old</li>
-                    <li>2 hours minimum technique at one or more dance studios: one hour of ballet 
-                        or ballet combo and one hour of another genre of dance.</li>
-                 </ul>
-                </div>
-            </div>
-        );
-    }
+        <div>
+          <h2>Dynamites Mini Team eligibility:</h2>
+          <ul>
+            <li>5-7 years old.</li>
+            <li>
+              2 hours minimum technique at one or more dance studios: one hour
+              of ballet or ballet combo and one hour of another genre of dance.
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AuditionEligibility;
