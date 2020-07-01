ec48fd8aa08c7ce024f177a3d5a07453d015d1f1
Jason Cabrera <cabrerajason7@gmail.com>
2020-07-01 00:14:16 -0700

    switched to tabs instead of tabpanes

--- src/pages/About/Gallery.js
+++ src/pages/About/Gallery.js
@@ -3,7 +3,7 @@ import Toolbar from "../../components/Toolbar";
 import GalleryImages from "../../components/GalleryImages.js";
 import Footer from '../../components/Footer.js';
 import '../../css/Gallery.css';
-import { Tabs, TabPane } from 'react-bootstrap';
+import { Tabs, Tab } from 'react-bootstrap';
 
 class Gallery extends Component {
   constructor(props) {
@@ -35,6 +35,8 @@ class Gallery extends Component {
     return(
       <div>
          <Toolbar />
+         <Tabs className="tabholder" fill defaultActiveKey="photos">
+           <Tab eventKey="photos" title="Photos">
              <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
              <div className="text-modal-container"> 
                <h1 className="tab-title"> Photo Gallery </h1>
@@ -42,7 +44,42 @@ class Gallery extends Component {
             <GalleryImages/>
             <Footer />
             </div>
+            </Tab>
          
+           <Tab eventKey="videos" title="Videos">
+            {/*Render first div if window is desktop size, render second div if tablet or mobile*/}
+            {
+            this.state.isDesktop ? 
+              <div>
+                <div style={{alignItems: "center", justifyContent: "center"}}>
+                  <div className="gallery-text-container"> 
+                    <h1 className="tab-title"> Video Gallery </h1>
+                    <img className="DPT-logo" src={require("../../images/logo1.png")}></img>
+                    <br />
+                    <p className="subscribe-text"> For more videos, click <a style={{color: '#FF4081'}} target="_blank" href="https://www.youtube.com/channel/UCLVld8eG5THi_R1MpLobU4g">here</a> to subscribe to our Youtube channel! </p>
+                  </div> 
+                </div>
+
+                <iframe className="youtube-iframe" src="https://www.youtube.com/embed/+lastest?list=PLCyEpHAXCjJMDXWsfMdWfD1NHh3zoYa1m" frameborder="0" allowFullScreen></iframe>
+                <Footer />
+              </div>
+            : 
+              <div> 
+                <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}> 
+                  <img className="DPT-logo" src={require("../../images/logo1.png")}></img>
+                  <h1 className="tab-title"> Video Gallery </h1>
+                </div>
+
+                <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>  
+                  <iframe className="youtube-iframe" src="https://www.youtube.com/embed/+lastest?list=PLCyEpHAXCjJMDXWsfMdWfD1NHh3zoYa1m" frameborder="0" allowFullScreen></iframe>
+                  <p className="subscribe-text"> For more videos, click <a style={{color: '#FF4081'}} target="_blank" href="https://www.youtube.com/channel/UCLVld8eG5THi_R1MpLobU4g">here</a> to subscribe to our Youtube channel! </p>
+                </div>  
+
+                {(window.innerWidth <= 600 && window.innerHeight < 900) ? <div className="bottom-absolute"> <Footer/> </div> : <Footer />}
+              </div>
+          }
+          </Tab>
+         </Tabs>
       </div>
     );
   }
