3ef157cf42200324aba6ea3a86d3380be9156beb
Jason Cabrera <cabrerajason7@gmail.com>
2020-07-01 19:21:03 -0700

    updated classNames, removed resize listener

--- src/pages/About/Gallery.js
+++ src/pages/About/Gallery.js
@@ -6,39 +6,14 @@ import '../../css/Gallery.css';
 import { Tabs, TabPane } from 'react-bootstrap';
 
 class Gallery extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-       isDesktop: true
-    };
-
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-  }
-
-  componentDidMount() {
-    this.updateWindowDimensions();
-    console.log(window.innerHeight, window.innerWidth);
-		window.addEventListener('resize', this.updateWindowDimensions);
-	}
-	
-	updateWindowDimensions() {
-		if (window.innerWidth > 800 && !this.state.isDesktop){
-			this.setState({ isDesktop: true });
-		}
-    
-    if(window.innerWidth <= 800 && this.state.isDesktop){
-			this.setState({ isDesktop: false });
-		}
-	}
   render() {
-
     return(
       <div>
          <Toolbar />
          <Tabs className="tabholder" fill defaultActiveKey="photos">
            <TabPane eventKey="photos" title="Photos">
              <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
-             <div className="text-modal-container"> 
+             <div className="photos-container"> 
                <h1 className="tab-title"> Photo Gallery </h1>
              </div> 
             <GalleryImages/>
@@ -49,7 +24,7 @@ class Gallery extends Component {
            <TabPane eventKey="videos" title="Videos">
               <div>
                 <div style={{alignItems: "center", justifyContent: "center"}}>
-                  <div className="gallery-text-container"> 
+                  <div className="videos-container"> 
                     <h1 className="tab-title"> Video Gallery </h1>
                     <img className="DPT-logo" src={require("../../images/logo1.png")}></img>
                     <br />
