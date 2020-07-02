a0de30338934c608115caf6b81815eccaf3fb32c
Dhanush <dhanunanjunda2@gmail.com>
2020-07-02 11:55:53 -0700

    minor fixes to global css

--- src/pages/About/Gallery.js
+++ src/pages/About/Gallery.js
@@ -39,7 +39,7 @@ class Gallery extends Component {
            <TabPane eventKey="photos" title="Photos">
              <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
              <div className="text-modal-container"> 
-               <h1 className="tab-title"> Photo Gallery </h1>
+               <h1 className="tab-title Pink"> Photo Gallery </h1>
              </div> 
             <GalleryImages/>
             <Footer />
@@ -53,7 +53,7 @@ class Gallery extends Component {
               <div>
                 <div style={{alignItems: "center", justifyContent: "center"}}>
                   <div className="gallery-text-container"> 
-                    <h1 className="tab-title"> Video Gallery </h1>
+                    <h1 className="tab-title Pink"> Video Gallery </h1>
                     <img className="DPT-logo" src={require("../../images/logo1.png")}></img>
                     <br />
                     <p className="subscribe-text"> For more videos, click <a style={{color: '#FF4081'}} target="_blank" href="https://www.youtube.com/channel/UCLVld8eG5THi_R1MpLobU4g">here</a> to subscribe to our Youtube channel! </p>
@@ -67,7 +67,7 @@ class Gallery extends Component {
               <div> 
                 <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}> 
                   <img className="DPT-logo" src={require("../../images/logo1.png")}></img>
-                  <h1 className="tab-title"> Video Gallery </h1>
+                  <h1 className="tab-title Pink"> Video Gallery </h1>
                 </div>
 
                 <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>  
