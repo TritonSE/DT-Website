72de7aa1907049e1343ee78bfbd5c648b92a8d99
Amrit-S <aksingh@ucsd.edu>
2020-07-03 23:58:58 -0700

    Updated donate button

--- src/components/WaysToGiveText.js
+++ src/components/WaysToGiveText.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import '../css/WaysToGive.css';
 import SponsorLetter from '../images/waytogive/Sponsor Letter 2019-20.pdf'
-import Amazon from '../images/waytogive/amazon smile.jpg';
-import CurrentFund from '../images/waytogive/Kendra Scott Fundraiser 1.png';
-import Instructions from '../images/waytogive/amazon smile instructions.jpg';
-import ModalImage from 'react-modal-image';
-import Footer from '../components/Footer.js';
 import SupportStates from '../components/SupportStates.js';
+import CurvedArrow from "react-curved-arrow";
 import '../css/Support.css';
+import {AiFillGift} from 'react-icons/ai';
+import {FaDonate} from 'react-icons/fa';
+import { AwesomeButton  } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 
 class WaysToGiveText extends Component {
     constructor(props){
@@ -33,7 +33,7 @@ class WaysToGiveText extends Component {
  				<p>
 					Dynamics Performance Team is a publicly Funded 501(c)(3) non-profit organization that depends on our generous donors, corporate sponsors, and friends. Your tax-deductible gift allows Dynamics to present quality performances, keep rates low, and offer scholarships to our dancers—affordability should not be an obstacle in the way of our dancers’ dreams. Your donation also allows for Dynamics dancers to compete, train at conventions, travel to nationals, and purchase costumes.
 				</p>
-				<p>
+				<p className="test">
 					To learn more about our individual or corporate giving programs and benefits, 
 					<a href = {SponsorLetter} target = "_blank"> <span style={{color: '#FF4081',fontStyle: 'normal',fontWeight: 'normal', textDecorationLine: 'underline'}}>please read our sponsor letter here </span></a>, or contact us at dynamicsperformanceteam@gmail.com. You can also give us a call at (408) 622-0249.
 				</p>
@@ -42,15 +42,34 @@ class WaysToGiveText extends Component {
 							
 							<SupportStates />
 					:
-							<button 
-								className="btn btn-secondary Donate-Button"
-								onClick={this.handleDonateButtonClicked}
-							>
-								Donate 
-							</button>
+						
+					<AwesomeButton
+						ripple
+						onPress={
+							this.handleDonateButtonClicked
+						  }
+						  size="large"
+						  
+					>
+						<FaDonate/>
+						I'll Support You!
+					</AwesomeButton>
+
+							// <button 
+							// 	className="btn btn-secondary Donate-Button"
+							// 	onClick={this.handleDonateButtonClicked}
+							// >
+							// 	{/* <IoIosCalculator/> */}
+							// 	<AiFillGift/>
+							// 	I'll Donate!
+								
+							// </button>
+
 					
 					}
 				</div>
+				{/* fromOffsetX={-300} middleX={-300} toOffsetX={-50} toOffsetY={-30} dynamicUpdate={true} width={4} */}
+				{/* <CurvedArrow fromSelector=".test" toSelector=".button-container" fromOffsetX={300} middleX={300} toOffsetX={50} toOffsetY={-30} dynamicUpdate={true} width={4}  /> */}
 					
             </div>
         	);
