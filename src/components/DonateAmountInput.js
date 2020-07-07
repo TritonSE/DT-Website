d3b6732b1727a256eac8a02a2cc07d1a4b71075d
Amrit-S <42155577+Amrit-S@users.noreply.github.com>
2020-07-07 11:04:18 -0700

    Updated Ways To Give Page (#109)
    * ways to give branch new
    
    * fixed slideshow
    
    * fixed some header issues
    
    * updated the font size
    
    * minor updates to ways-to-give
    
    * now fully merged
    
    * mobile with all buttons working
    
    * made ipad changes + recommended ones w/ added footer
    
    * made a mailchimp function
    
    * updated mobile version
    
    * fix for the iphone 5 issues
    
    * minor fixes
    
    * new format
    
    * slight fix for ipad
    
    * New proceed style
    
    * Fixed state bleading error
    
    * Mostly done version - footer now working for desktop
    
    * Updated donate button
    
    * Cleaned up some code
    
    * mobile footer space change
    
    * fixed console warnings
    
    Co-authored-by: nvanny <nvanny@ucsd.edu>
    Co-authored-by: Jason Cabrera <cabrerajason7@gmail.com>

---
+++ src/components/DonateAmountInput.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Input, Label } from 'semantic-ui-react';
+import '../css/Support.css';
+
+class DonateAmountInput extends React.Component {
+    render() {
+        return(
+            <div style={{display:"flex", flexDirection:"column"}}>
+                 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+                    <Input
+                    action={{
+                        color: 'teal',
+                        labelPosition: 'right',
+                        icon: 'cart',
+                        content: 'Donate',
+                        onClick: this.props.handleAmountChange
+                        }
+                    }
+                    type="number" 
+                    actionPosition='right'
+                    size={"mini"}
+                    placeholder='5.00'
+                    label="$"
+                    defaultValue={this.props.amount === '' ? null:this.props.amount}
+                    error={this.props.displayError}
+                    onChange={this.props.handleUpdateAmount}
+                />
+                {this.props.displayError && 
+                    <Label basic color='red' pointing="above">
+                        {this.props.errorMsg}
+                    </Label>
+                }
+            </div>
+        );
+    }
+}
+
+export default DonateAmountInput;
--- src/components/WaysToGiveText.js
+++ src/components/WaysToGiveText.js
@@ -1,89 +1,60 @@
 import React, { Component } from 'react';
 import '../css/WaysToGive.css';
 import SponsorLetter from '../images/waytogive/Sponsor Letter 2019-20.pdf'
-import Amazon from '../images/waytogive/amazon smile.jpg';
-import Instructions from '../images/waytogive/amazon smile instructions.jpg';
-import ModalImage from 'react-modal-image';
+import SupportStates from '../components/SupportStates.js';
+import '../css/Support.css';
+import {FaDonate} from 'react-icons/fa';
+import { AwesomeButton  } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 
 class WaysToGiveText extends Component {
     constructor(props){
 		super(props);
-		this.myRef = React.createRef();
-		this.imageRef = React.createRef();
-		this.state = {width: 0, height: 0, isMobile: false};
-		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+		this.state = {donateButtonClicked: false};
+		this.handleDonateButtonClicked = this.handleDonateButtonClicked.bind(this);
 	}
 
-	componentDidMount() {
-        window.scrollTo(0,0);
-		this.updateWindowDimensions();
-		window.addEventListener('resize', this.updateWindowDimensions);
+	handleDonateButtonClicked = () =>{
+		this.setState({
+			donateButtonClicked: true
+		});
 	}
 
-	updateWindowDimensions() {
-		if (window.innerWidth > 600) {
-			this.setState({isMobile: false});
-		}
-		else if (window.innerWidth <= 600) {
-			this.setState({isMobile: true});
-		}
-		this.setState({width: window.innerWidth, height: window.innerHeight});
-	}
 
-	componentWillUnmount() {
-		window.removeEventListener('resize', this.updateWindowDimensions);
-	}
     render() {
-        if(!this.state.isMobile) {
         return (
             <div className = "bodytxt">
                 <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'></link>
                 <p> 
-                Dynamics Performance Team is proud to be part of the local arts community in the Bay Area for the past 15 years. We are committed to giving youth and young adults the opportunity to train in versatile genres and grow their passion for dance. Performing Arts are an essential part of the community, and with your help, we can continue to offer training to all dancers regardless of financial background.
+                	Dynamics Performance Team is proud to be part of the local arts community in the Bay Area for the past 15 years. We are committed to giving youth and young adults the opportunity to train in versatile genres and grow their passion for dance. Performing Arts are an essential part of the community, and with your help, we can continue to offer training to all dancers regardless of financial background.
+ 				</p>
+ 				<p>
+					Dynamics Performance Team is a publicly Funded 501(c)(3) non-profit organization that depends on our generous donors, corporate sponsors, and friends. Your tax-deductible gift allows Dynamics to present quality performances, keep rates low, and offer scholarships to our dancers—affordability should not be an obstacle in the way of our dancers’ dreams. Your donation also allows for Dynamics dancers to compete, train at conventions, travel to nationals, and purchase costumes.
+				</p>
+				<p>
+					To learn more about our individual or corporate giving programs and benefits, 
+					<a href = {SponsorLetter} target = "_blank" rel="noopener noreferrer"> <span style={{color: '#FF4081',fontStyle: 'normal',fontWeight: 'normal', textDecorationLine: 'underline'}}>please read our sponsor letter here </span></a>, or contact us at dynamicsperformanceteam@gmail.com. You can also give us a call at (408) 622-0249.
+				</p>
+				<div className="states-container">
+					{ this.state.donateButtonClicked ?
+							
+							<SupportStates />
+					:
+						
+					<AwesomeButton
+						ripple
+						onPress={this.handleDonateButtonClicked}
+						size="large"
+					>
+						<FaDonate/>
+						I'll Support You!
+					</AwesomeButton>
 
- </p>
- <p>
-Dynamics Performance Team is a publicly Funded 501(c)(3) non-profit organization that depends on our generous donors, corporate sponsors, and friends. Your tax-deductible gift allows Dynamics to present quality performances, keep rates low, and offer scholarships to our dancers—affordability should not be an obstacle in the way of our dancers’ dreams. Your donation also allows for Dynamics dancers to compete, train at conventions, travel to nationals, and purchase costumes.
-</p>
-<p>
-To learn more about our individual or corporate giving programs and benefits, 
-<a href = {SponsorLetter} target = "_blank" rel="noopener noreferrer"> <span className="Pink" style={{fontStyle: 'normal',fontWeight: 'normal', textDecorationLine: 'underline'}}>please read our sponsor letter here </span></a>, or contact us at dynamicsperformanceteam@gmail.com. You can also give us a call at (408) 622-0249.
-</p>
-<div className="amazon">
-					<p>Amazon Smile Donations</p>
+					}
 				</div>
-				<ModalImage
-					className = "smile"
-					small = {Amazon}
-					large = {Amazon}
-				/>
-				<ModalImage
-					className = "instructions"
-					small = {Instructions}
-					medium = {Instructions}
-				/>
+					
             </div>
-        );
-        }
-        else {
-        return (
-            <div className = "bodytxt">
-                <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'></link>
-                <p> 
-                Dynamics Performance Team is proud to be part of the local arts community in the Bay Area for the past 15 years. We are committed to giving youth and young adults the opportunity to train in versatile genres and grow their passion for dance. Performing Arts are an essential part of the community, and with your help, we can continue to offer training to all dancers regardless of financial background.
-
- </p>
- <p>
-Dynamics Performance Team is a publicly Funded 501(c)(3) non-profit organization that depends on our generous donors, corporate sponsors, and friends. Your tax-deductible gift allows Dynamics to present quality performances, keep rates low, and offer scholarships to our dancers—affordability should not be an obstacle in the way of our dancers’ dreams. Your donation also allows for Dynamics dancers to compete, train at conventions, travel to nationals, and purchase costumes.
-</p>
-<p>
-To learn more about our individual or corporate giving programs and benefits, 
-<a href = {SponsorLetter} target = "_blank" rel="noopener noreferrer"> <span className="Pink" style={{fontStyle: 'normal',fontWeight: 'normal', textDecorationLine: 'underline'}}>please read our sponsor letter here </span></a>, or contact us at dynamicsperformanceteam@gmail.com. You can also give us a call at (408) 622-0249.
-</p>
-                 
-                 </div>
-        );
-        }
+        	);
     }
 }
 
