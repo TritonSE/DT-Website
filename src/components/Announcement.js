703e969ee22a7814f578402353dabd9fd470afca
Jason Cabrera <cabrerajason7@gmail.com>
2020-07-20 22:10:45 -0700

    added padding to announcement text

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -8,7 +8,7 @@ class Announcement extends React.Component {
     render() {
         return (
             <div className="Announcement-Container"> 
-                <p className="Announcement-Text"> Join us <a className="Announcement-Text" target="_blank" rel="noopener noreferrer" style={{textDecoration: "underline"}} href="https://www.youtube.com/channel/UCLVld8eG5THi_R1MpLobU4g"> here</a> for our virtual show "Legends" on August 1st at 6pm! </p>
+                <p className="Announcement-Text Announcement-Text-Padding"> Join us <a className="Announcement-Text" target="_blank" rel="noopener noreferrer" style={{textDecoration: "underline"}} href="https://www.youtube.com/channel/UCLVld8eG5THi_R1MpLobU4g"> here</a> for our virtual show "Legends" on August 1st at 6pm! </p>
             </div>
         )
     }
